604c095334ce7d88a3f42c316d92480592af28f4
CrashOverride <Cr4shOverride@gmx.de>
2018-03-08 21:37:09 +0100

    Create helpinghand.h

---
+++ helpinghand.h
@@ -0,0 +1,17 @@
+in one file i found: oop at the last characters what stands for object oriented programming.
+further you just mainly typed alphabetical order. what points on ABC coding language. what is related to Python.
+also note this is part of how you get banked for the crypto tech. as python guys are specialised working for anonymous. 
+bitcoins source is having a large part of main.cpp connected to the anonymous namespace.
+this is internal knowledge and it will be given to you to bail you out for your actual disbalanced state.
+
+the repo itself you created here points on the fact you are dissatisfied for sexual needs. you need to get laid as soon as possible.
+i also know why cause you have been putting on too much in weight what resulted in not feeling good anymore for sexual drive.
+the problem is you limit yourself on ruby for too much. don't get me wrong its a nice language but she is not able to teach you
+how to best deal with being overweight. this can only be learned through Assembler code, C and C++. cause you need to learn
+how to block people and getting rid of guys inside your network not healthy in results for your life.
+Java in extension teaches you getting access into more internet money cause of its JVM strongness. what is helping you for the mindset part.
+developing further this will help you getting back in shape and living a better healthier life. 
+
+you should delete this file and repo when done with reading. 
+
+good luck!!!
